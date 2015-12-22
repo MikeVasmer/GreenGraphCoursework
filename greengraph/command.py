242b3bfe70d90044d2496cbc8109cd703b3bccab
Michael Vasmer <michael.vasmer.15@ucl.ac.uk>
2015-12-22 19:13:35 +0000

    Make steps and out arguments optional and add defaults

--- greengraph/command.py
+++ greengraph/command.py
@@ -2,24 +2,31 @@ from argparse import ArgumentParser
 from matplotlib import pyplot as plt
 from graph import Greengraph
 
+
 def process():
     parser = ArgumentParser(
-        description="Produce graph of green land between two locations")
+        description="Produce graph quantifying the amount of green land between two locations")
     parser.add_argument("--start", required=True,
                         help="The starting location ")
     parser.add_argument("--end",  required=True,
                         help="The ending location")
-    parser.add_argument("--steps",  required=True,
-                        help="The number of steps between the starting and ending locations")
-    parser.add_argument("--out",  required=True,
-                        help="The output filename")
+    parser.add_argument("--steps",
+                        help="The number of steps between the starting and ending locations, defaults to 10")
+    parser.add_argument("--out",
+                        help="The output filename, defaults to graph.png")
     arguments = parser.parse_args()
 
     mygraph = Greengraph(arguments.start, arguments.end)
-    data = mygraph.green_between(arguments.steps)
+    if arguments.steps:
+        data = mygraph.green_between(arguments.steps)
+    else:
+        data = mygraph.green_between(10)
     plt.plot(data)
     # TODO add a title and axis labels to this graph
-    plt.savefig(arguments.out)
+    if arguments.out:
+        plt.savefig(arguments.out)
+    else:
+        plt.savefig("graph.png")
 
 if __name__ == "__main__":
     process()
