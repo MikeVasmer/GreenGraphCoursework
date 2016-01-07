15b2dc2356648f7a190682b8b378ab1309b5c4c8
Michael Vasmer <michael.vasmer.15@ucl.ac.uk>
2016-01-07 21:02:01 +0000

    Add function to test constructor of Map class

--- greengraph/test/test_map.py
+++ greengraph/test/test_map.py
@@ -3,21 +3,22 @@ import numpy as np
 from nose.tools import assert_equal
 from mock import patch
 import os
+import requests
 
-@patch('requests.get')
-@patch('matplotlib.image.imread')
-@patch('StringIO.StringIO')
+lat = 50
+lon = 50
+size = (10,10)
+
+@patch("requests.get")
+@patch("matplotlib.image.imread")
+@patch("StringIO.StringIO")
 def test_green(mock_get,mock_imread,mock_StringIO):
 
     def assert_images_equal(r,g,b,checkArray):
         testMap.pixels = np.dstack((r,g,b))
         np.testing.assert_array_equal(testMap.green(threshold),checkArray)
 
-    lat = 50
-    lon = 50
     testMap = Map(lat,lon)
-
-    size = (10,10)
     trueArray = np.ones(size,dtype=bool)
     falseArray = np.zeros(size,dtype=bool)
     threshold = 1
@@ -41,15 +42,12 @@ def test_green(mock_get,mock_imread,mock_StringIO):
     blue = np.zeros(size)
     assert_images_equal(red,green,blue,trueArray)
 
-@patch('requests.get')
-@patch('matplotlib.image.imread')
-@patch('StringIO.StringIO')
+@patch("requests.get")
+@patch("matplotlib.image.imread")
+@patch("StringIO.StringIO")
 def test_count_green(mock_get,mock_imread,mock_StringIO):
 
-    lat = 50
-    lon = 50
     testMap = Map(lat,lon)
-    size = (10,10)
 
     #Test that the number sum of the green pixels with values greater than the values of both the red and blue pixels is zero for an empty image (zeros everywhere)
     green = np.zeros(size)
@@ -62,3 +60,21 @@ def test_count_green(mock_get,mock_imread,mock_StringIO):
     green = np.ones(size)
     testMap.pixels = np.dstack((red,green,blue))
     assert_equal(testMap.count_green(),size[0]*size[1])
+
+@patch("matplotlib.image.imread")
+@patch("StringIO.StringIO")
+def test_Map_init(mock_imread,mock_StringIO):
+    #Test that requests.get() is called with the expected default parameters when creating an instance of the Map class
+    with patch.object(requests,'get') as mock_get:
+        testMap = Map(lat,lon)
+        mock_get.assert_called_with(
+        "http://maps.googleapis.com/maps/api/staticmap?",
+        params={
+            "sensor":"false",
+            "zoom":10,
+            "size":"400x400",
+            "center":str(lat)+","+str(lon),
+            "style":"feature:all|element:labels|visibility:off",
+            "maptype":"satellite"
+        }
+        )
