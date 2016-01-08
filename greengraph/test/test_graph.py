aa6bbe315c753671a654e28370e8e07a56f40152
Michael Vasmer <michael.vasmer.15@ucl.ac.uk>
2016-01-08 13:04:43 +0000

    Update location_sequence method test to use new fixture file location_sequence.yaml

--- greengraph/test/test_graph.py
+++ greengraph/test/test_graph.py
@@ -1,6 +1,8 @@
 from greengraph.map import Map
 from greengraph.graph import Greengraph
 from mock import patch
+from mock import Mock
+import numpy as np
 import geopy
 from nose.tools import assert_equal
 from nose.tools import assert_almost_equal
@@ -11,7 +13,6 @@ start = "London"
 end = "Durham"
 
 def test_Greengraph_init():
-    #Test instance of Greengraph class instantiated correctly
     with patch.object(geopy.geocoders,'GoogleV3') as mock_GoogleV3:
         test_Greengraph = Greengraph(start,end)
         #Test that GoogleV3 is called with the correct parameters
@@ -22,22 +23,26 @@ def test_Greengraph_init():
 
 def test_geolocate():
     #Test that the geolocate method returns the correct latitude and longitude coordinates for various places
-        with open(os.path.join(os.path.dirname(__file__),"fixtures","geolocate.yaml")) as fixtures_file:
-            test_Greengraph = Greengraph(start,end)
-            fixtures = yaml.load(fixtures_file)
+    with open(os.path.join(os.path.dirname(__file__),"fixtures","geolocate.yaml")) as fixtures_file:
+        fixtures = yaml.load(fixtures_file)
+        test_Greengraph = Greengraph(start,end)
         for fixture in fixtures:
             assert_almost_equal(
             test_Greengraph.geolocate(fixture.pop("place")),(fixture.pop("lat"),fixture.pop("lon"))
-            )
+        )
 
 def test_location_sequence():
     #Test that the location_sequence method computes the steps between two coordinates correctly
     test_Greengraph = Greengraph(start,end)
-    test_sequence = test_Greengraph.location_sequence((0,0),(50,50),6)
-    expected_results = [0.,10.,20.,30.,40.,50.]
-    i = 0
-    for result in expected_results:
-        test_Greengraph = Greengraph(start,end)
-        assert_equal(test_sequence[i][0],result)
-        assert_equal(test_sequence[i][1],result)
-        i += 1
+
+    with open(os.path.join(os.path.dirname(__file__),"fixtures","location_sequence.yaml")) as fixtures_file:
+        fixtures = yaml.load(fixtures_file)
+        for fixture in fixtures:
+            expected_results = fixture.pop("result")
+            i = 0
+            test_sequence = test_Greengraph.location_sequence(
+            (fixture.pop("startlat"),fixture.pop("startlon")),(fixture.pop("endlat"),fixture.pop("endlon")),fixture.pop("steps"))
+            for result in expected_results:
+                assert_equal(test_sequence[i][0],result)
+                assert_equal(test_sequence[i][1],result)
+                i += 1
