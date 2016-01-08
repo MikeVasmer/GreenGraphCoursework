13d473079af2e1b193c765b9900e5f38819b5699
Michael Vasmer <michael.vasmer.15@ucl.ac.uk>
2016-01-08 11:13:29 +0000

    Add test for geolocate method using new yaml fixture

--- greengraph/test/test_graph.py
+++ greengraph/test/test_graph.py
@@ -3,11 +3,15 @@ from greengraph.graph import Greengraph
 from mock import patch
 import geopy
 from nose.tools import assert_equal
+from nose.tools import assert_almost_equal
+import os
+import yaml
 
 start = "London"
 end = "Durham"
 
 def test_Greengraph_init():
+    #Test instance of Greengraph class instantiated correctly
     with patch.object(geopy.geocoders,'GoogleV3') as mock_GoogleV3:
         test_Greengraph = Greengraph(start,end)
         #Test that GoogleV3 is called with the correct parameters
@@ -15,3 +19,13 @@ def test_Greengraph_init():
         #Test that the start and end fields are initialised correctly
         assert_equal(test_Greengraph.start,start)
         assert_equal(test_Greengraph.end,end)
+
+def test_geolocate():
+    #Test that the geolocate method returns the correct latitude and longitude coordinates for various places
+    with open(os.path.join(os.path.dirname(__file__),"fixtures","geolocate.yaml")) as fixtures_file:
+        test_Greengraph = Greengraph(start,end)
+        fixtures = yaml.load(fixtures_file)
+        for fixture in fixtures:
+            assert_almost_equal(
+            test_Greengraph.geolocate(fixture.pop("place")),(fixture.pop("lat"),fixture.pop("lon"))
+            )
